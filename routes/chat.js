97ec04d3569615e8adb169aee4267ca7b4db6db1
unknown <Wulize@172105861@qq.com>
2022-01-07 17:58:08 +0800

    feat:新增用户状态接口

--- routes/chat.js
+++ routes/chat.js
@@ -4,7 +4,8 @@ const Koa = require('koa')
 const tool = require('../util/constant')
 const websockify = require('koa-websocket') //cg
 const app = websockify(new Koa()); //cg
-let ctxs = {}; //cg
+let ctxs = {}; //所用用户
+let online = []; // 在线用户数组
 app.listen(3001); //cg
 router.prefix('/chat')
 
@@ -12,13 +13,30 @@ app.ws.use(async(ctx, next) => {
     /* 每打开一个连接就往 上线文数组中 添加一个上下文 */
     let { id } = ctx.query;
     ctxs[id] = ctx;
+
+    /*这部分负责未读消息的处理*/
     // 首次连接将未读消息推送给用户
     const unreadMsg = await getdata('unreadMsg', { receiver: id });
     if (unreadMsg.length)
         unreadMsg.forEach(element => {
             ctxs[id].websocket.send(JSON.stringify(element));
         });
+    // 消息推送给用户之后就从未读信息表中删除
     await deleteData('unreadMsg', { receiver: id });
+
+    /*负责用户在线离线状态的处理*/
+    online = Object.keys(ctxs);
+
+    online.forEach(async(item) => {
+        let myFriends = (await getdata('friendIList', { userName: item }))[0].friendList || ['请先添加好友'];
+        // 求交集，给出在线好友列表
+        let onlineFriend = myFriends.filter((val) => new Set(online).has(val));
+        if (onlineFriend !== []) {
+            ctxs[item].websocket.send(JSON.stringify({ type: "isOnline", onlineFriend }))
+        }
+    })
+
+    /*处理用户发送到后台的信息*/
     console.log(id + '  connect');
     ctx.websocket.on("message", async(message) => {
         let data = JSON.parse(message); //处理数据
@@ -39,11 +57,23 @@ app.ws.use(async(ctx, next) => {
             }
         }
     });
+
     ctx.websocket.on("close", (message) => {
         /* 连接关闭时, 清理 上下文数组, 防止报错 */
         let key = tool.findKey(ctx, ctxs);
         console.log(key + "关闭了链接");
         delete ctxs[key];
+        /*用户下线时更新好友的在线名单*/
+        online.splice(online.indexOf(key), 1);
+        online.forEach(async(item) => {
+            let myFriends = (await getdata('friendIList', { userName: item }))[0].friendList || ['请先添加好友'];
+            // 求交集，给出在线好友列表
+            let onlineFriend = myFriends.filter((val) => new Set(online).has(val));
+            if (onlineFriend !== []) {
+                ctxs[item].websocket.send(JSON.stringify({ type: "isOnline", onlineFriend }))
+            }
+        })
+
     });
 });
 
